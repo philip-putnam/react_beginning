2d048e628742c3b1bf82ea0f324958d68f0ec91d
Philip Putnam <putnamp989@gmail.com>
2017-06-08 21:31:03 -0700

    update App.js to stateless ES6 syntax

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,10 @@
 var React = require('react');
 
-class AppComponent extends React.Component {
-  render() {
-    return <div>
-      <h1>Hello!</h1>
-      <p>Welcome to my site!</p>
-    </div>
-  }
+const AppComponent = () => {
+  return <div>
+    <h1>Hello!</h1>
+    <p>Welcome to my site!</p>
+  </div>
 }
 
 module.exports = AppComponent;
