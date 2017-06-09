005a80bcaa75d2d6742f05878c3532ad4bb801a9
Philip Putnam <putnamp989@gmail.com>
2017-06-08 21:27:18 -0700

    update for ES6 class syntax in App.js

--- app/components/App.js
+++ app/components/App.js
@@ -1,13 +1,12 @@
 var React = require('react');
 
-var App = React.createClass({
-  render: function () {
-    return ( <div>
+class AppComponent extends React.Component {
+  render() {
+    return <div>
       <h1>Hello!</h1>
       <p>Welcome to my site!</p>
     </div>
-    )
   }
-});
+}
 
-module.exports = App;
+module.exports = AppComponent;
