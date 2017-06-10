aa4aa52b78924855a2302b55287dea852fad6c81
Philip Putnam <putnamp989@gmail.com>
2017-06-10 12:33:36 -0700

    add decrementScore function w/ functionality onClick '-' button

--- app/components/App.js
+++ app/components/App.js
@@ -29,10 +29,16 @@ var Counter = React.createClass({
     })
   },
 
+  decrementScore: function(e) {
+    this.setState({
+      score: (this.state.score - 1),
+    })
+  },
+
   render: function() {
     return (
       <div className='counter'>
-        <button className='counter-action decrement'> - </button>
+        <button className='counter-action decrement' onClick={this.decrementScore}> - </button>
         <div className='counter-score'> {this.state.score} </div>
         <button className='counter-action increment' onClick={this.incrementScore}> + </button>
       </div>
