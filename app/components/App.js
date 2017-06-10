8f09a64793ff0658c7f6823fb4e8a4b407c61a93
Philip Putnam <putnamp989@gmail.com>
2017-06-09 20:52:35 -0700

    begin React Treehouse tract/courses, new app.css, and begin scoreboard component

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,37 @@
 var React = require('react');
 
-const AppComponent = () => {
-  return <div>
-    <h1>Hello!</h1>
-    <p>Welcome to my site!</p>
-  </div>
-}
+function Application() {
+  return (
+    <div className='application'>
+      <div className='header'>
+        <h1>Scoreboard</h1>
+      </div>
 
-module.exports = AppComponent;
+      <div className='players'>
+        <div className='player'>
+          <div className='player-name'>
+            Jim Hoskins
+          </div>
+          <div className='player-score'>
+            <div className='counter'>
+              <button className='counter-action decrement'> - </button>
+              <button className='counter-action increment'> + </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+
+
+
+// const AppComponent = () => {
+//   return <div>
+//     <h1>Hello!</h1>
+//     <p>Welcome to my site!</p>
+//   </div>
+// }
+
+module.exports = Application;
