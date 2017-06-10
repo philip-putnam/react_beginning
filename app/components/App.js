a63f3f8eedfa98842ca15b9014cb1d2104792bfe
Philip Putnam <putnamp989@gmail.com>
2017-06-09 21:38:10 -0700

    continue with Treehouse React, 'decomposing application'

--- app/components/App.js
+++ app/components/App.js
@@ -1,31 +1,61 @@
 var React = require('react');
 
-function Application() {
+function Header(props) {
   return (
-    <div className='application'>
-      <div className='header'>
-        <h1>Scoreboard</h1>
-      </div>
+    <div className='header'>
+      <h1>{props.title}</h1>
+    </div>
+  );
+}
 
-      <div className='players'>
-        <div className='player'>
-          <div className='player-name'>
-            Jim Hoskins
-          </div>
-          <div className='player-score'>
-            <div className='counter'>
-              <button className='counter-action decrement'> - </button>
-              <button className='counter-action increment'> + </button>
-            </div>
-          </div>
+Header.propTypes = {
+  title: React.PropTypes.string.isRequired,
+};
+
+function Player(props) {
+  return (
+    <div className='player'>
+      <div className='player-name'>
+        {props.name}
+      </div>
+      <div className='player-score'>
+        <div className='counter'>
+          <button className='counter-action decrement'> - </button>
+          <div className='counter-score'> {props.score} </div>
+          <button className='counter-action increment'> + </button>
         </div>
       </div>
+    </div>
+  );
+}
+
+Player.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  score: React.PropTypes.number.isRequired,
+}
+
+
+function Application(props) {
+  return (
+    <div className='scoreboard'>
+    <Header title={props.title} />
+
+      <div className='players'>
+        <Player name='Jim Hoskins' score={31}/>
+        <Player name='Philip Putnam' score={900}/>
+      </div>
+
     </div>
   )
 };
 
+Application.propTypes = {
+  title: React.PropTypes.string,
+};
 
-
+Application.defaultProps = {
+  title: "Scoreboard"
+};
 
 // const AppComponent = () => {
 //   return <div>
