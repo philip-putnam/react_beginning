5c85b732ff2250acf5942675385f45c750ea5aa8
Philip Putnam <putnamp989@gmail.com>
2017-06-10 13:41:34 -0700

    add players prop to Header and pass it through in Application class, create totalPlayers and totalPoints variables in Stats using reduce()

--- app/components/App.js
+++ app/components/App.js
@@ -2,26 +2,35 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 function Stats(props) {
+  var totalPlayers = props.players.length;
+  var totalPoints = props.players.reduce(function(total, player) {
+    return total + player.score;
+  }, 0);
+
   return (
     <table className='stats'>
       <tbody>
         <tr>
           <td>Players:</td>
-          <td>2</td>
+          <td>{totalPlayers}</td>
         </tr>
         <tr>
           <td>Total Points:</td>
-          <td>123</td>
+          <td>{totalPoints}</td>
         </tr>
       </tbody>
     </table>
   )
 }
 
+Stats.propTypes = {
+  players: React.PropTypes.array.isRequired,
+};
+
 function Header(props) {
   return (
     <div className='header'>
-      <Stats />
+      <Stats players={props.players} />
       <h1>{props.title}</h1>
     </div>
   );
@@ -29,6 +38,7 @@ function Header(props) {
 
 Header.propTypes = {
   title: React.PropTypes.string.isRequired,
+  players: React.PropTypes.array.isRequired,
 };
 
 function Counter(props) {
@@ -96,7 +106,7 @@ var Application = React.createClass({
   render: function() {
     return (
       <div className='scoreboard'>
-        <Header title={this.props.title} />
+        <Header title={this.props.title} players={this.state.players} />
 
         <div className='players'>
           {this.state.players.map(function(player, index) {
