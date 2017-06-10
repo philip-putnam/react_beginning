bde19ce501398cd62b4654be32c8bafb99b44792
Philip Putnam <putnamp989@gmail.com>
2017-06-10 14:16:29 -0700

    add stateful Stopwatch class, render it in Header in App.js

--- app/components/App.js
+++ app/components/App.js
@@ -3,6 +3,21 @@ var ReactDOM = require('react-dom');
 
 var nextId = 5;
 
+var Stopwatch = React.createClass({
+  render: function() {
+    return (
+      <div className='stopwatch'>
+        <h2>Stopwatch</h2>
+        <div className='stopwatch-time'>
+          0
+        </div>
+        <button>Start</button>
+        <button>Reset</button>
+      </div>
+    );
+  }
+});
+
 var AddPlayerForm = React.createClass({
   propTypes: {
     onAdd: React.PropTypes.func.isRequired,
@@ -69,6 +84,7 @@ function Header(props) {
     <div className='header'>
       <Stats players={props.players} />
       <h1>{props.title}</h1>
+      <Stopwatch />
     </div>
   );
 }
